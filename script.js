e92ef27412af8a2b2a7dbb68e17fff623a3bf2a5
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-06-16 01:10:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function load() {
+	var body = document.querySelector('body');
+	body.textContent = 'DOM load success';
+}
+document.addEventListener('DOMContentLoaded', load);
